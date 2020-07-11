4330a27010ae651e9cd0efd5404b68d1d7c241e2
Rodrigo García <yo@rodrigogarcia.me>
2020-07-11 13:36:17 +0200

    Add instructions scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,4 +1,6 @@
 extends Node2D
 
-func _ready():
-	pass
+func _input(_event):
+	if Input.is_action_pressed('ui_accept'):
+		var instructionsScene = 'res://scenes/instructions.tscn'
+		get_tree().change_scene(instructionsScene)
