89220b5dd2bcaeaba2c8cde7f12befe38936dca1
Rodrigo García <yo@rodrigogarcia.me>
2020-07-11 13:41:59 +0200

    Handle change scene error

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,4 +3,5 @@ extends Node2D
 func _input(_event):
 	if Input.is_action_pressed('ui_accept'):
 		var instructionsScene = 'res://scenes/instructions.tscn'
-		get_tree().change_scene(instructionsScene)
+		if get_tree().change_scene(instructionsScene) != OK:
+			print('error loading instructions scene')
