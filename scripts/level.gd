71e5418156a9343849422f6da27e33e2470c95e5
Rodrigo García <yo@rodrigogarcia.me>
2020-07-12 23:32:07 +0200

    Add sepia filter

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+onready var sepia = get_node("sepia-filter")
 func _ready():
 	match Global.level:
 		1:
-			modulate = Color(0.64, 0.46, 0.28, 1.0)
+			sepia.visible = true
 		2:
 			modulate = Color(0.65, 0.77, 0.81, 1.0)
+			sepia.visible = false
 		_:
 			modulate = Color(1.0, 1.0, 1.0, 1.0)
+			sepia.visible = false
