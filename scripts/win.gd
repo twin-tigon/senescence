e69ae066f8f9815d641c61d349cdd4af966bdcf5
Sebastian Munera <sfmunera@gmail.com>
2020-07-12 16:20:39 -0700

    Animate winning scene with ghost

--- scripts/win.gd
+++ scripts/win.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 onready var tween1 = get_node("tween1")
 onready var tween2 = get_node("tween2")
+onready var tween3 = get_node("tween3")
 onready var tweenEnd = get_node("tweenEnd")
 onready var timer = get_node("timer")
 
 var isEnd = false
 
 func _ready():
+	$granny.modulate.a = 0
 	tween1.interpolate_property($sentence1, "modulate:a", 0.0, 1.0, 4.0, 1, Tween.EASE_IN, 0)
 	tween1.start()
 
@@ -21,8 +23,17 @@ func _on_tween1_tween_completed(_object, _key):
 func _on_tween2_tween_completed(_object, _key):
 	if isEnd:
 		return
+	
+	tween3.interpolate_property($granny, "modulate:a", 0.0, 1.0, 4.0, 1, Tween.EASE_IN, 0)
+	tween3.start()
+	
+func _on_tween3_tween_completed(_object, _key):
+	if isEnd:
+		return
 
 	timer.start()
+	
+
 
 func _on_timer_timeout():
 	isEnd = true
@@ -32,6 +43,8 @@ func _on_timer_timeout():
 	tween1.start()
 	tween2.interpolate_property($sentence2, "modulate:a", 1.0, 0.0, 2.0, 1, Tween.EASE_IN, 0)
 	tween2.start()
+	tween3.interpolate_property($granny, "modulate:a", 1.0, 0.0, 2.0, 1, Tween.EASE_IN, 0)
+	tween3.start()
 
 	tweenEnd.interpolate_property($end, "modulate:a", 0.0, 1.0, 6.0, 1, Tween.EASE_IN, 0)
 	tweenEnd.start()
